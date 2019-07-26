a7c9fb4d147952ed7cbae1854eee54fbf491ed08
Nima Saed <nima.saed@me.com>
2019-07-26 10:19:01 +0800

    Added wpa service, lock service and more packages and settings in arch setup

--- scripts/archSetup.sh
+++ scripts/archSetup.sh
@@ -2,51 +2,46 @@ sudo pacman -Syu
 
 sudo pacman -S \
 git \
+i3-wm \
+i3blocks \
+i3lock \
 vim \
 feh \
-rofi \
 acpi \
 xorg-xinit \
 xorg-server \
 xorg-xrandr \
-ttf-freefont \
+xorg-xbacklight \
 ttf-hack \
 firefox \
 otf-font-awesome \
 tmux \
-ranger \
-python \
-python-pip \
+vifm \
 bash-completion \
-cmatrix \
 openssh \
 imagemagick \
 scrot \
 wget \
-arc-gtk-theme \
-arc-icon-theme \
-nautilus \
 alsa-utils \
 sysstat \
 screenfetch \
 go \
 virtualbox \
-python-pyqt5 \
 cpupower \
 tlp \
-vulkan-intel \
-libva-intel-driver \
 unclutter \
-w3m \
-qutebrowser \
-pandoc \
-acpi_call \
-ethtool \
-smartmontools \
-x86_energy_perf_policy \
 mupdf \
 highlight \
-qtile
+hsetroot \
+mpv \
+dmenu \
+pacman-contrib \
+mesa \
+vulkan-intel \
+xf86-video-intel \
+docker \
+docker-compose \
+libva-intel-driver
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
@@ -58,8 +53,6 @@ rm -Rf yay
 
 yay -S --nocleanmenu --nodiffmenu \
 skypeforlinux-stable-bin \
-otf-san-francisco \
-urxvt-resize-font-git \
 virtualbox-ext-oracle \
 dropbox \
 dropbox-cli
@@ -69,3 +62,14 @@ sudo systemctl enable tlp.service
 sudo systemctl enable tlp-sleep.service
 sudo systemctl mask systemd-rfkill.service
 sudo systemctl mask systemd-rfkill.socket
+
+sudo cp ../i3/lock.service /etc/systemd/system/
+sudo systemctl enable lock.service
+
+sudo cp ../wifi/wpa_supplicant.service /etc/systemd/system/
+sudo systemctl enable wpa_supplicant.service
+
+sudo sed -i 's/dockerd -H/dockerd --data-root=\/home\/.docker -H/g' /usr/lib/systemd/system/docker.service
+sudo systemctl enable docker
+
+sudo usermod -a -G wheel,docker,vboxusers nima
