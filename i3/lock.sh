38ef35546a23c4fd91a294e3e278e78efb38af09
Nima Saed <nima.saed@me.com>
2019-10-04 10:44:54 +0800

    New lock for lockscreen

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,11 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o nounset # Treat unset variables as an error
+
+# Get latest image from bing images folder
+IMG=$HOME/Dropbox/bing_images/$(ls -r $HOME/Dropbox/bing_images/ | grep -m 1 bing)
+
+# Set lock icon
 ICON=~/.config/i3/lock-icon.png
-TMPBG=/tmp/screen.png
-secondScreen off
-sleep 0.1
-scrot -z /tmp/screen.png
-convert $TMPBG -scale 10% -scale 1000% $TMPBG
-convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
-i3lock -e -i $TMPBG
-rm -f $TMPBG
+
+# Set path to save the modified image
+TMPBG=/tmp/lock
+
+# Add the lock icon to the center of the bing image
+convert $IMG $ICON -gravity center -composite -matte -format PNG $TMPBG.png
+
+# Start i3lock with black color, disable mouse and set the created image
+i3lock -c 000000 -e -i $TMPBG.png
+
+# To give some time to i3lock to load the image before suspend happen
 sleep 0.2
