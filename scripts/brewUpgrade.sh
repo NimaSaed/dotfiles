827425f309e3b2d1709bfe49f69ed0501563fdce
Nima Saed <nima@nmsd.xyz>
2024-07-16 10:32:15 +0200

    Added color

--- scripts/brewUpgrade.sh
+++ scripts/brewUpgrade.sh
@@ -1,17 +1,58 @@
 #!/usr/bin/env bash
+
 set -o nounset # Treat unset variables as an error
 
-echo "[*] [$(date +%Y-%m-%d\ @\ %H:%M:%S)] Updating brew database: ";
-brew update
-echo "[*] [$(date +%Y-%m-%d\ @\ %H:%M:%S)] Database is updated";
-echo "[*] [$(date +%Y-%m-%d\ @\ %H:%M:%S)] Upgrading Cli packages";
-brew upgrade;
-echo "[*] [$(date +%Y-%m-%d\ @\ %H:%M:%S)] Cli packages are updated";
+function color(){
+    # $1 the color light colors 31 - 38 dark colors 91 - 97
+    # $2 normal:0 bold:1 underline:4 background:7
+    # $3 string
+    printf "\e[%s;49;%sm %s \e[0m" $2 $1 "$3"
 
-echo "[*] [$(date +%Y-%m-%d\ @\ %H:%M:%S)] Getting list of brew cask packages installed"
+}
+
+function indent() { sed 's/^/    /'; }
+
+color 32 1 "[*]"
+color 34 0 "[$(date +%Y-%m-%d\ @\ %H:%M:%S)]"
+color 95 7 "Updating brew database"
+echo
+
+brew update 2>&1 | indent
+
+color 32 1 "[*]"
+color 34 0 "[$(date +%Y-%m-%d\ @\ %H:%M:%S)]"
+color 95 7 "Database is updated"
+echo
+
+color 32 1 "[*]"
+color 34 0 "[$(date +%Y-%m-%d\ @\ %H:%M:%S)]"
+color 95 7 "Upgrading Cli packages"
+echo
+
+
+brew upgrade;
+
+
+color 32 1 "[*]"
+color 34 0 "[$(date +%Y-%m-%d\ @\ %H:%M:%S)]"
+color 95 7 "Cli packages are updated"
+echo
+
+color 32 1 "[*]"
+color 34 0 "[$(date +%Y-%m-%d\ @\ %H:%M:%S)]"
+color 95 7 "Getting list of brew cask packages installed"
+echo
+
 cask_list=$(brew list --cask)
 
-echo "[*] [$(date +%Y-%m-%d\ @\ %H:%M:%S)] Upgrading cask packages"
-brew upgrade --cask $cask_list
+color 32 1 "[*]"
+color 34 0 "[$(date +%Y-%m-%d\ @\ %H:%M:%S)]"
+color 95 7 "Upgrading cask packages"
+echo
 
-echo "[*] [$(date +%Y-%m-%d\ @\ %H:%M:%S)] Done"
+brew upgrade --cask $cask_list 2>&1 | indent
+
+color 32 1 "[*]"
+color 34 0 "[$(date +%Y-%m-%d\ @\ %H:%M:%S)]"
+color 95 7 "Done"
+echo
